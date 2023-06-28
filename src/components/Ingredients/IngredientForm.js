fb7f330b9393152efb8bf4e6e7e89cfd7efff320
Rodrigo Reis <rreisp@gmail.com>
2023-06-28 10:48:34 -0300

    Passing State Data Across Components

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -9,7 +9,7 @@ const IngredientForm = React.memo(props => {
 
   const submitHandler = event => {
     event.preventDefault();
-    // ...
+    props.onAddIngredient({ title: enteredTitle, amount: enteredAmount });
   };
 
   return (
@@ -18,21 +18,25 @@ const IngredientForm = React.memo(props => {
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input type="text"
+            <input
+              type="text"
               id="title"
               value={enteredTitle}
               onChange={event => {
                 setEnteredTitle(event.target.value);
-              }} />
+              }}
+            />
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input type="number"
+            <input
+              type="number"
               id="amount"
               value={enteredAmount}
               onChange={event => {
                 setEnteredAmount(event.target.value);
-              }} />
+              }}
+            />
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
