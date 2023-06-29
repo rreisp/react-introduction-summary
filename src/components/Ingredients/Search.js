630b4317ae63db399da466457b98391c61bcc991
Rodrigo Reis <rreisp@gmail.com>
2023-06-29 14:02:40 -0300

    Cleaning Up with useEffect()

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -9,26 +9,29 @@ const Search = React.memo(props => {
   const inputRef = useRef();
 
   useEffect(() => {
-    setTimeout(() => {
-      if (enteredFilter === inputRef.current.value) { 
+    const timer = setTimeout(() => {
+      if (enteredFilter === inputRef.current.value) {
         const query = enteredFilter.length === 0
-        ? ''
-        : `?orderBy="title"&equalTo="${enteredFilter}"`;
-      fetch('https://react-hooks-update-3bd31-default-rtdb.firebaseio.com/ingredients.json' + query)
-        .then(response => response.json())
-        .then(responseData => {
-          const loadedIngredients = [];
-          for (const key in responseData) {
-            loadedIngredients.push({
-              id: key,
-              title: responseData[key].title,
-              amount: responseData[key].amount
-            });
-          }
-          onLoadIngredients(loadedIngredients);
-        });
+          ? ''
+          : `?orderBy="title"&equalTo="${enteredFilter}"`;
+        fetch('https://react-hooks-update-3bd31-default-rtdb.firebaseio.com/ingredients.json' + query)
+          .then(response => response.json())
+          .then(responseData => {
+            const loadedIngredients = [];
+            for (const key in responseData) {
+              loadedIngredients.push({
+                id: key,
+                title: responseData[key].title,
+                amount: responseData[key].amount
+              });
+            }
+            onLoadIngredients(loadedIngredients);
+          });
       }
     }, 500);
+    return () => {
+      clearTimeout(timer);
+    };
   }, [enteredFilter, onLoadIngredients, inputRef]);
 
   return (
